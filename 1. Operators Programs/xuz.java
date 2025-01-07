363d420c2fa2d5da3ba4ce3b415feda1db412796
Ashwan-Dath <145241850+Ashwan-Dath@users.noreply.github.com>
2025-01-07 22:41:18 +0530

    Update 1. Operators Programs

---
+++ 1. Operators Programs/xuz.java
@@ -0,0 +1,150 @@
+// Java Program to print pattern
+// Number-increasing reverse pyramid
+import java.util.*;
+
+public class GeeksForGeeks {
+    // Function to demonstrate pattern
+    public static void printPattern(int n)
+    {
+        int i, j;
+        // outer loop to handle number of rows
+        for (i = n; i >= 1; i--) {
+            // inner loop to handle number of columns
+            for (j = 1; j <= i; j++) {
+                // printing column values upto the row
+                // value.
+                System.out.print(j + " ");
+            }
+
+            // print new line for each row
+            System.out.println();
+        }
+    }
+
+    // Driver Function
+    public static void main(String args[])
+    {
+        int n = 6;
+        printPattern(n);
+    }
+}
+
+
+
+// Java Program to print pattern
+// Number-changing pyramid
+import java.util.*;
+
+// Java code for printing pattern
+public class GeeksForGeeks {
+    // Function to demonstrate pattern
+    public static void printPattern(int n)
+    {
+        int i, j;
+        int num = 1;
+        // outer loop to handle number of rows
+        for (i = 1; i <= n; i++) {
+            // inner loop to handle number of columns
+            for (j = 1; j <= i; j++) {
+                // printing value of num in each iteration.
+                System.out.print(num + " ");
+                // increasing the value of num.
+                num++;
+            }
+
+            // printing new line for each row
+            System.out.println();
+        }
+    }
+
+    // Driver Function
+    public static void main(String args[])
+    {
+        int n = 6;
+        printPattern(n);
+    }
+}
+
+
+
+
+
+
+// Java Program to print pattern
+// Number-changing pyramid
+import java.util.*;
+
+// Java code for printing pattern
+public class GeeksForGeeks {
+    // Function to demonstrate pattern
+    public static void printPattern(int n)
+    {
+        int i, j;
+        int num = 1;
+        // outer loop to handle number of rows
+        for (i = 1; i <= n; i++) {
+            // inner loop to handle number of columns
+            for (j = 1; j <= i; j++) {
+                // printing value of num in each iteration.
+                System.out.print(num + " ");
+                // increasing the value of num.
+                num++;
+            }
+
+            // printing new line for each row
+            System.out.println();
+        }
+    }
+
+    // Driver Function
+    public static void main(String args[])
+    {
+        int n = 6;
+        printPattern(n);
+    }
+}
+
+
+
+
+
+
+// Java Program to print pattern
+// Palindrome triangle
+import java.util.*;
+
+public class GeeksForGeeks {
+    // Function to demonstrate pattern
+    public static void printPattern(int n)
+    {
+        int i, j;
+
+        // outer loop to handle number of rows
+        for (i = 1; i <= n; i++) {
+            // inner loop to print the spaces
+            for (j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+
+            // inner loop to print the first part
+            for (j = i; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+
+            // inner loop to print the second part
+            for (j = 2; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+
+            // printing new line for each row
+            System.out.println();
+        }
+    }
+
+    // Driver Function
+    public static void main(String args[])
+    {
+        int n = 6;
+        printPattern(n);
+    }
+}2
