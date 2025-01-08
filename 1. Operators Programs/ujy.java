170433c4f0b5efd7343f50336b579cc4f6a0c481
Ashwan-Dath <145241850+Ashwan-Dath@users.noreply.github.com>
2025-01-08 21:17:58 +0530

    Update 1. Operators Programs

---
+++ 1. Operators Programs/ujy.java
@@ -0,0 +1,17 @@
+//lr is lower range and ur is upper range  
+int lr = 0, ur = 0;  
+Scanner scan = new Scanner(System.in);  
+System.out.print("Enter lower range: ");  
+//reads an integer (lr) from the user  
+lr = scan.nextInt();  
+System.out.print("Enter upper range: ");  
+//reads an integer (ur) from the user  
+ur = scan.nextInt();  
+System.out.println("The Sunny number from "+ lr + " to "+ ur+" are: ");  
+for(int i=lr; i<=ur; i++)   
+{  
+//calling user-defined number  
+if(isSunny(i))  
+System.out.print(i +" ");  
+}  
+}  
