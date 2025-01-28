848536a096a704de4580e773ea997f9e713c886e
Ashwan-Dath <145241850+Ashwan-Dath@users.noreply.github.com>
2025-01-28 22:18:27 +0530

    Update blabla.java

--- blabla.java
+++ blabla.java
@@ -72,3 +72,22 @@ System.out.println("The given number is not a sunny number.");
 }   
 }   
 }  
+
+
+class PalindromeExample{  
+ public static void main(String args[]){  
+  int r,sum=0,temp;    
+  int n=454;//It is the number variable to be checked for palindrome  
+  
+  temp=n;    
+  while(n>0){    
+   r=n%10;  //getting remainder  
+   sum=(sum*10)+r;    
+   n=n/10;    
+  }    
+  if(temp==sum)    
+   System.out.println("palindrome number ");    
+  else    
+   System.out.println("not palindrome");    
+}  
+}  
