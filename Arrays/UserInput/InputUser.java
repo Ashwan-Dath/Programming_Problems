c10a3216c77ce1b8959c5d364a91dd5d7ae82868
Ashwan-Dath <komashdat@gmail.com>
2025-06-25 23:07:07 +0530

    UserInput

---
+++ Arrays/UserInput/InputUser.java
@@ -0,0 +1,67 @@
+package UserInput;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class InputUser {
+    
+
+    static  Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+//        System.out.print(Arrays.toString(IntegerArrayInput()));
+//        IntegerArrayInput2D();
+//        StringArrayInput();
+//        System.out.print(Arrays.toString(StringArrayInput()));
+// This is to push the code to github
+    }
+
+    public static int[][] IntegerArrayInput2D(){
+        System.out.print("Enter the row of the Array : ");
+        int row =  sc.nextInt();
+        System.out.print("Enter the column of the Array : ");
+        int col = sc.nextInt();
+
+
+        int [][] arr = new int[row][col];
+        // Giving col is optional, row is mandatory
+
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                System.out.print("Enter the value of "+(i+1)+" x "+(j+1)+" : ");
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+//        for(int i = 0; i < row; i++){
+//            for(int j = 0; j < col; j++){
+//                System.out.print(arr[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+        return arr;
+    }
+
+    public static int[] IntegerArrayInput(){
+        System.out.print("Enter the Size of the Array : ");
+        int size = sc.nextInt();
+
+        int []arr =  new int[size];
+        for(int i = 0; i < size; i++){
+            System.out.printf("Enter the %d element in the Array : ", (i + 1));
+            arr[i] = sc.nextInt();
+        }
+        return arr;
+    }
+
+    public  static String[] StringArrayInput(){
+        System.out.print("Enter the Size of the Array : ");
+        int size = sc.nextInt();
+
+        String []arr =  new String[size];
+        for(int i = 0; i < size; i++){
+            System.out.printf("Enter the %d element in the Array : ", (i + 1));
+            arr[i] = sc.next();
+        }
+        return arr;
+    }
+}
